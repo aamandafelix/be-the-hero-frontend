0edd08928c14f3cd4b3e4d966ed13008a6cbf341
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-04-01 18:51:28 -0300

    Create profile page

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 function Routes() {
   return (
@@ -10,6 +11,7 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Logon} />
         <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
       </Switch>
     </BrowserRouter>
   );
