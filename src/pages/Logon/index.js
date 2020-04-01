5c87d4f3f60277973643e24644d7d9979b07d141
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-04-01 20:05:55 -0300

    Create login functionality

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi'
+
+import api from '../../services/api';
 
 import './styles.css'
 
@@ -8,15 +10,38 @@ import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 function Logon() {
+  const history = useHistory();
+  
+  const [id, setId] = useState('');
+
+  async function handleLogin(event) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post('sessions', { id });
+      
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Falha no login, tente novamente');
+    }
+  }
+
   return (
    <div className="logon-container">
      <section className="form">
       <img src={logoImg} alt="Be The Hero" />
 
-      <form>
+      <form onSubmit={handleLogin}>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="Sua ID" />
+        <input 
+          placeholder="Sua ID"
+          value={id}
+          onChange={event => setId(event.target.value)}
+        />
         <button className="button" type="submit">Entrar</button>
 
         <Link className="back-link" to="/register">
